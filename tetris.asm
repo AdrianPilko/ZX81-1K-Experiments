0c15b580342a5b8c9abac459e19f73aa671d0444
AdrianPilko <50658445+AdrianPilko@users.noreply.github.com>
2022-04-24 09:33:23 +0100

    made screen full hieght and started design in comments in tetris

--- tetris.asm
+++ tetris.asm
@@ -2,7 +2,7 @@
 ;;; zx81 1K code
 ;;; This is minimal code that will run on the limited memory of standard unexpanded 1K zx81
 ;;; It's a clone of tetris (in case that wasn't clear from filename;)
-;;; The code heavily!! dependant on the definition of the screen memory in screen.asm
+;;; The code heavily!! dependant on the definition of the screen memory in screenTetris.asm
 ;;;;;;;;;;;;;;;;;;;;;
 
 ; all the includes came from  https://www.sinclairzxworld.com/viewtopic.php?t=2186&start=40
@@ -21,55 +21,33 @@
 title_screen_txt
 	DEFB	_Z,_X,_8,_1,__,_1,_K,__,_T,_E,_T,_R,_I,_S,$ff
 
-	
+;; intro screen
 intro_title
-	; no need for clear screen as screen.asm has already set everything to zero
-	ld bc,0
-	ld de,title_screen_txt
+	; no need for clear screen as screenTetris.asm has already set 
+    ; everything to zero    
+	ld bc,0                     ; printstring from offset from DF__CC stored in bc
+	ld de,title_screen_txt      ; load text into de for printstring
 	call printstring	
-	ld e, 9
+	
+	
+;; initialise "variables" and memory
 
-scroll_loop
-	ld b, e				; row set for PRINTAT
-	ld c, 0				; column set for PRINTAT
-	push de	
-	call PRINTAT		; ROM routine to set current cursor position, from row b and column e
-	ld a, e
-	and 1				; odd even of e countdown (0 or 1)
-	jp nz, printEvenLine	
-	ld bc,4				; loop 4 times, each loop prints 2 characters to give full 8 by 8 chessboard
-	ld a, 0
-innerLoop1	
-	ld a, 8				; character code for grey square
-	call PRINT			; call ROM routine to print a character
-	ld a, 128	 		; character code for black square
-	call PRINT			; call ROM routine to print a character
-	dec bc
-	ld a, c
-	cp 0
-	jp nz, innerLoop1
-	jp loopOuterControl
-printEvenLine
-	ld bc,4
-innerLoop2	
-	ld a, 128			; character code for black square
-	call PRINT			; call ROM routine to print a character
-	ld a, 8				; character code for grey square
-	call PRINT			; call ROM routine to print a character
-	dec bc
-	ld a, c
-	cp 0
-	jp nz, innerLoop2
-loopOuterControl
-	pop de
-	dec e
-	ld a, e
-	cp 1
-	jp nz,scroll_loop	
-	ret	; this does return to basic, but due to the screen setup it crashes if run again
-		; the zx81 will attempt to change the screen memory dynamically as it lists the program
-		; so all the offsets we setup here based on a small screen config will be
-		; wrong
+;; main game loop
+
+;; generate shape
+
+;; user input to retate shape
+
+;; scroll shapes down
+
+;; detect if line(s) has/have been completed and remove, and drop remaining down
+
+;; increase score if line(s) removed
+
+;; detect if game over condition met, it collision between shape entering at top
+
+
+	jp intro_title   ; never return to basic, new game always starts from title screen
 		
 ; this prints at top any offset (stored in bc) from the top of the screen D_FILE
 printstring
@@ -87,5 +65,5 @@ printstring_end
 	ret
 
 #include "line2.asm"
-#include "screen.asm"      			; definition of the screen memory, in colapsed version for 1K        
+#include "screenTetris.asm"      			; definition of the screen memory, in colapsed version for 1K        
 #include "endbasic.asm"

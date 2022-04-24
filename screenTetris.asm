4414c203e2c57647f3cb7d396e7d396e4a00d16b
AdrianPilko <50658445+AdrianPilko@users.noreply.github.com>
2022-04-24 10:30:02 +0100

    screen update, fixed title

--- screenTetris.asm
+++ screenTetris.asm
@@ -1,7 +1,7 @@
 ;ZX81 screens
 ; Display file (ZX81 screen) - low res screen 
 Display        	; empty (collapsed) DFILE on zx81 1K consists of the 25 chr$118 (hex $76)
-	DEFB $76,0,0,0,0,0,0,0,0,0 ; 0 first chr$118 marks the start of DFILE     
+	DEFB $76,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0; 0 first chr$118 marks the start of DFILE     
 	DEFB $76,0,0,0,0,0,0,0,0,0 ; 1
 	DEFB $76,0,0,0,0,0,0,0,0,0 ; 2  
 	DEFB $76,0,0,0,0,0,0,0,0,0 ; 3  
@@ -24,7 +24,7 @@ Display        	; empty (collapsed) DFILE on zx81 1K consists of the 25 chr$118 
 	DEFB $76,0,0,0,0,0,0,0,0,0 ; 20
 	DEFB $76,0,0,0,0,0,0,0,0,0 ; 21
 	DEFB $76,0,0,0,0,0,0,0,0,0 ; 22
-	DEFB $76,0,0,0,0,0,0,0,0,0 ; 23   (24th line counting from zero)
+    DEFB $76,0,0,0,0,0,0,0,0,0 ; 23
 	DEFB $76
 
                 

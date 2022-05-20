db674f0909f51fbc3b85ea9dbd2dee8094cec3cd
AdrianPilko <50658445+AdrianPilko@users.noreply.github.com>
2022-05-20 13:54:07 +0100

    tidy up of code no functional changes

--- tetris16K.asm
+++ tetris16K.asm
@@ -31,7 +31,10 @@
 #define SHAPE_CHAR_1 136        ; grey square
 #define BOTTOM 22
 
-	jp intro_title		        ; jump to print title text
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	jp intro_title		; main entry poitn to the code ships the memory definitions
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 	
 ;; the underscore characters here are mapped onto real zx81 
 ;; characters in charcodes.asm, they are more human readble 
@@ -45,8 +48,8 @@ currentShape
 shapes      ; Shapes are known as Tetromino (see wikipedia), use 8 bits per shape
             ; base shape 2 column * 4 rows to make logic easier, interpreted as such in the code and definition 
             ;  "square"   "L"    "straight"   "T"  "skew left" "skew right"
-            ;       00     00       10        00     00          00    1100   10
-            ;       00     10       10        01     10          01    0110   10
+            ;       00     00       10        00     00          00   
+            ;       00     10       10        01     10          01   
             ;       11     10       10        11     11          11    
             ;       11     11       10        01     01          10    
 ; shape definition (bit packed)
@@ -54,7 +57,7 @@ shapes      ; Shapes are known as Tetromino (see wikipedia), use 8 bits per shap
    DEFB %00001111,  %00101011,%10101010,%00011101,%00101101, %00011110   ; should be drawn vertically
    DEFB %01100110,  %00101110,%00001111,%11100100,%01101100, %11000110   ; should be drawn horiz
    DEFB %00001111,  %11010100,%10101010,%10111000,%10110100, %00011110   ; should be drawn vertically
-   DEFB %01100110,  %11101000,%00001111,%01001110,%01101100, %11000110  ; should be drawn horiz   
+   DEFB %01100110,  %11101000,%00001111,%01001110,%01101100, %11000110   ; should be drawn horiz   
 
 ;   DEFB             %00010111,           %00101110,    
 
@@ -109,7 +112,7 @@ speedUp
     DEFB 0
 ;; intro screen
 intro_title    
-	; screenTetris.asm has already set everything including the title
+    ; screenTetris16K.asm has already set everything including the title
     ; clear the play area (is need for all after first game as play area will be filled with previous blocks
     ld b, BOTTOM
     ld a, 11    
@@ -117,7 +120,7 @@ intro_title
 initPlayAreaLoop        
     push bc
     ld bc, (initScreenIndex)
-	ld de,screen_area_blank_txt
+    ld de,screen_area_blank_txt
     call printstring    
     ld a,(initScreenIndex)    
     add a, 10            
@@ -157,24 +160,24 @@ deleteOldShape
     call drawShape
     ld a, 0
     ld (deleteShapeFlag),a          ;  clear the flag
-    
+
     ; read the keyboard input and adust the offset     
-	ld a, KEYBOARD_READ_PORT_SHIFT_TO_V			; read keyboard shift to v
-	in a, (KEYBOARD_READ_PORT)					; read from io port	
-	bit 1, a
-	; check bit set for key press left  (Z)
-	jp z, shapeLeft								; jump to move shape left
-	ld a, KEYBOARD_READ_PORT_SPACE_TO_B			; read keyboard space to B
-	in a, (KEYBOARD_READ_PORT)					; read from io port		
-	bit 2, a									; check bit set for key press right (M)
-	jr z, shapeRight							; jump to move shape right	
-	jp noShapeMove								; dropped through to no move
+    ld a, KEYBOARD_READ_PORT_SHIFT_TO_V			; read keyboard shift to v
+    in a, (KEYBOARD_READ_PORT)					; read from io port	
+    bit 1, a
+    ; check bit set for key press left  (Z)
+    jp z, shapeLeft								; jump to move shape left
+    ld a, KEYBOARD_READ_PORT_SPACE_TO_B			; read keyboard space to B
+    in a, (KEYBOARD_READ_PORT)					; read from io port		
+    bit 2, a									; check bit set for key press right (M)
+    jr z, shapeRight							; jump to move shape right	
+    jp noShapeMove								; dropped through to no move
 
 shapeRight
 
     ;; need to account for rotation when checking if shape can go further to right
     ; and also special case for vertical drawn straight shape to go fully to right
-    
+
     ld a, (rotationCount)
     and %00000001       ;; work out if rotation count is odd or even    
     jr nz, handleShapeRightForHorizontal             ;; if odd then treat as a horizontal shape
@@ -193,7 +196,7 @@ shapeRight
     ld a, (currentShapeOffset)
     cp 20
     jp z, handleShapeRight_StrVert
-    
+
     ld a, (shapeTrackLeftRight)
     dec a
     cp 1
@@ -202,7 +205,7 @@ shapeRight
     ld a, (shape_row_index)
     inc a                  
     ld (shape_row_index), a    
-	jp noShapeMove	
+    jp noShapeMove	
 
 handleShapeRight_StrVert
     ld a, (shapeTrackLeftRight)
@@ -213,7 +216,7 @@ handleShapeRight_StrVert
     ld a, (shape_row_index)
     inc a                  
     ld (shape_row_index), a    
-	jp noShapeMove	
+    jp noShapeMove	
     
 handleShapeRightForHorizontal
 
@@ -231,7 +234,7 @@ handleShapeRightForHorizontal
     ld a, (currentShapeOffset)
     cp 20
     jp z, handleShapeRight_StrHoriz
-    
+
     ld a, (shapeTrackLeftRight)
     dec a
     cp 2
@@ -240,7 +243,7 @@ handleShapeRightForHorizontal
     ld a, (shape_row_index)
     inc a                  
     ld (shape_row_index), a    
-	jp noShapeMove	
+    jp noShapeMove	
     
 handleShapeRight_StrHoriz
     ld a, (shapeTrackLeftRight)
@@ -251,14 +254,14 @@ handleShapeRight_StrHoriz
     ld a, (shape_row_index)
     inc a                  
     ld (shape_row_index), a    
-	jp noShapeMove	
+    jp noShapeMove	
 shapeLeft
     ld a, (shapeTrackLeftRight)
     inc a
     cp 8
     jp z, noShapeMove 
     ld (shapeTrackLeftRight),a 
-    
+
     ld a, (shape_row_index)
     dec a     
     ld (shape_row_index), a 
@@ -267,11 +270,11 @@ noShapeMove
 
     ;;; read the rotate shape after the left right is done,
     ;; we draw the shape then next time the delete shape code runs will delete rotated
-	ld a, KEYBOARD_READ_PORT_SHIFT_TO_V			; read keyboard shift to v
-	in a, (KEYBOARD_READ_PORT)					; read from io port	
-	bit 2, a
-	; check bit set for key press rotate  use X key 
-	jp nz, drawShapeHook    
+    ld a, KEYBOARD_READ_PORT_SHIFT_TO_V			; read keyboard shift to v
+    in a, (KEYBOARD_READ_PORT)					; read from io port	
+    bit 2, a
+    ; check bit set for key press rotate  use X key 
+    jp nz, drawShapeHook    
     ld a, (rotationCount)
     inc a
     cp 4
@@ -286,7 +289,7 @@ noShapeMove
     ld a, (shape_row)
     sub 2
     ld (shape_row),a
-    
+
     call drawShape
     jp  preWaitloop
 
@@ -297,33 +300,33 @@ storeIncrementedRotation
     ld (currentShapeOffset),a
     call drawShape
     jp  preWaitloop
-    
+
 drawShapeHook    
     call drawShape
 preWaitloop	
-	ld a, (score_mem_tens)
-	cp 153
-	jr z, addOneToHund
-	jr skipAddHund
+    ld a, (score_mem_tens)
+    cp 153
+    jr z, addOneToHund
+    jr skipAddHund
 addOneToHund
-	ld a, 0
-	ld (score_mem_tens), a
+    ld a, 0
+    ld (score_mem_tens), a
     ld a, (score_mem_hund)             
-	add a, 1
-	daa    
-	ld (score_mem_hund), a
+    add a, 1
+    daa    
+    ld (score_mem_hund), a
 skipAddHund	
 
 printScoreInGame
-	ld bc, 1
+    ld bc, 1
     ld de, score_mem_tens
-	call printNumber    
-    
-	ld bc, 4            ; print the current difficulty level (lower number is harder)
-    ld de, speedUp    
-	call printNumber    
+    call printNumber    
 
-    	
+    ld bc, 4            ; print the current difficulty level (lower number is harder)
+    ld de, speedUp    
+    call printNumber    
+
+        
     ld bc, (speedUp) 
     ld hl, $0fff
     adc hl, bc
@@ -340,15 +343,13 @@ waitloop
     cp 1                            ; if flagForBottomHit is set then this will set zero flag
                                     ; so we need to check if rows are complete first
     jp z, checkForCompleteLinesInit
-  
+
     ld a, (shape_row)
     inc a
     ld (shape_row),a    
     cp BOTTOM                            ; only gets here if no shapes at bottom
     jp nz, dropLoop
     jp main                   
-    
-    
        
 checkForCompleteLinesInit   
     ld a, 11                        ; offset to first block in screen play area 
@@ -379,11 +380,11 @@ afterSetlineNOTComplete
     ld (checkColIndex), a
     cp 7
     jp nz, checkLine                ; always complete check loop fully
-    
+
     ld a, (lineCompleteFlag)
     cp 1
-	jp z,removelineIsComplete
-    
+    jp z,removelineIsComplete
+
     jp checkCompleteLoopInc
     
 setlineNOTComplete
@@ -401,8 +402,8 @@ removelineIsComplete
     ld (speedUp),a
 
     ld a,(score_mem_tens)				; add one to score, scoring is binary coded decimal (BCD)
-	add a,1	
-	daa									; z80 daa instruction realigns for BCD after add or subtract  
+    add a,1	
+    daa									; z80 daa instruction realigns for BCD after add or subtract  
     ld (score_mem_tens),a				; add one to score, scoring is binary coded decimal (BCD)
     ; move all lives about this down by one
     ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -412,21 +413,21 @@ removelineIsComplete
 playAreaShuffle
     ld hl, (DF_CC)
     ld bc, (copyOfCheckColOffsetStartRow)    	; checkColOffsetStartRow is an offset from DF_CC, 
-                                        ; not address of screen memory
-	add hl,bc
-    ld (lineRemoved), hl                ; lineRemoved is now the 16bit copy of address the 
-                                        ; start of play area for the line romoved    
-    ld a, (copyOfCheckColOffsetStartRow)      ; subtract 10 from checkColOffsetStartRow
-    sub 10                              ; this gets us the offset to the previous line...
-    ld (copyOfCheckColOffsetStartRow),a      ; subtract 10 from checkColOffsetStartRow
-    ld hl, $00                          ; have to zero this here otherwise left overs in lineToSuffleFrom top 8bits 
+                                                ; not address of screen memory
+    add hl,bc
+    ld (lineRemoved), hl                        ; lineRemoved is now the 16bit copy of address the 
+                                                ; start of play area for the line romoved    
+    ld a, (copyOfCheckColOffsetStartRow)        ; subtract 10 from checkColOffsetStartRow
+    sub 10                                      ; this gets us the offset to the previous line...
+    ld (copyOfCheckColOffsetStartRow),a         ; subtract 10 from checkColOffsetStartRow
+    ld hl, $00                                  ; have to zero this here otherwise left overs in lineToSuffleFrom top 8bits 
     ld (lineToSuffleFrom), hl
-    ld (lineToSuffleFrom) , a           ; ...the line we're shuffling down from 
+    ld (lineToSuffleFrom) , a                   ; ...the line we're shuffling down from 
     ld bc,(lineToSuffleFrom)
     ld hl, (DF_CC)
-	add hl,bc
-    ld (lineToSuffleFrom), hl           ; lineToSuffleFrom is a 16 bit value now the offset 
-                                        ; from start of  screen memory
+    add hl,bc
+    ld (lineToSuffleFrom), hl                   ; lineToSuffleFrom is a 16 bit value now the offset 
+                                                ; from start of  screen memory
     ld hl, (lineRemoved)            
     ld bc, (lineToSuffleFrom)
     ld e, 0
@@ -435,7 +436,7 @@ loopFor_7_Shuffle
     ld (hl), a   ;; this instruction crashes!!       ; load screen position at hl with a    
     inc hl                              ; move position in screen memory we're writing to on one 
     inc bc                              ; move the position we're moving from on one
-                                        ; loop count down to zero                                        
+                                    ; loop count down to zero                                        
     inc e
     ld a, e
     cp 7
@@ -445,7 +446,7 @@ loopFor_7_Shuffle
     ld a,(copyOfCheckColOffsetStartRow)  
     cp 21
     jp nz, playAreaShuffle
-   
+
     pop bc
     pop de         
     pop hl
@@ -476,8 +477,8 @@ gameOver
     call printstring	
     ld bc, $ffff
     ld a, 0
-	ld (score_mem_tens), a
-	ld (score_mem_hund), a	    
+    ld (score_mem_tens), a
+    ld (score_mem_hund), a	    
 waitloopRetryGame
     dec bc
     ld a,b
@@ -487,53 +488,50 @@ waitloopRetryGame
    
 ; this prints at top any offset (stored in bc) from the top of the screen D_FILE
 printstring
-	ld hl,(DF_CC)
-	add hl,bc	
+    ld hl,(DF_CC)
+    add hl,bc	
 printstring_loop
-	ld a,(de)
-	cp $ff
-	jp z,printstring_end
-	ld (hl),a
-	inc hl
-	inc de
-	jr printstring_loop
+    ld a,(de)
+    cp $ff
+    jp z,printstring_end
+    ld (hl),a
+    inc hl
+    inc de
+    jr printstring_loop
 printstring_end	
-	ret  
+    ret  
 
 printNumber
-	ld hl,(DF_CC)
-	add hl,bc	
+    ld hl,(DF_CC)
+    add hl,bc	
 printNumber_loop
-	ld a,(de)
-  	push af ;store the original value of a for later
-	and $f0 ; isolate the first digit
-	rra
-	rra
-	rra
-	rra
-	add a,$1c ; add 28 to the character code
-	ld (hl), a
+    ld a,(de)
+    push af ;store the original value of a for later
+    and $f0 ; isolate the first digit
+    rra
+    rra
+    rra
+    rra
+    add a,$1c ; add 28 to the character code
+    ld (hl), a
     inc hl
-	pop af ; retrieve original value of a
-	and $0f ; isolate the second digit
-	add a,$1c ; add 28 to the character code
-	ld (hl), a      
-	ret  
+    pop af ; retrieve original value of a
+    and $0f ; isolate the second digit
+    add a,$1c ; add 28 to the character code
+    ld (hl), a      
+    ret  
 
 drawShape  
-        ;ld bc, 45
-        ;ld de, (shape_row_index)
-        ;call printNumber    
     ld a,(deleteShapeFlag)     
     cp 1
     jp z, dontIncrementShapeRowIndex    ;; if we're deleting shape then skip increment shape_row_index
-    
+
     ld a, (shape_row_index)
     add a, 10                  ; always need ten as the offset, the left right just adds bit to this   
     ld (shape_row_index), a
 
 dontIncrementShapeRowIndex
-    
+
     ld a, (currentShapeOffset)
     ld hl, shapes
     ld d, 0                            
@@ -548,7 +546,7 @@ dontIncrementShapeRowIndex
     ;; this will only draw shape at top need to add current position offset
     add hl, de                          ; to where we want to draw shape
     ld c, %10000000                     ; mask for shape (initialised, but will be rotated  )
-    
+
     ;; alter loop counts when rotating so draw horizontal or vertical, not just vertical                                    l
     ld a, (rotationCount)
     and %00000001       ;; work out if rotation count is odd or even    
@@ -579,14 +577,14 @@ drawShapeInner
     ; detect if hl is already drawn on (ie a block already in that location, if so stop
     ;; we also need to draw the shape from the bottom upwards, because we want to detect the collision earlier
     ;; and actually we should to a "trial draw of shape then if no collisions actually draw it!!
-    
+
     push hl
     ;ld de, (displayLineIncrement)
     ld de, 10
     add hl, de
     ld a, (hl)
     and SHAPE_CHAR_0                      ; this will result in "true" if block exists already in that position    
-    cp 0                                
+    ;cp 0                                ; don't need cp 0, as the and sets the flags (saved 2bytes wooo!)
     pop hl
                                         
     jp z, drawTheDamnSquare             ; set a flag to say if move shape one more down will be collision
@@ -601,10 +599,10 @@ drawTheDamnSquare
     ld a,(deleteShapeFlag)     ;; if we're deleting the old shape then don't draw anything
     cp 1
     jp z, loadBlank
-    
+
     ld a, (currentShapeOffset)
     and %00000011
-    cp 0
+    ;cp 0                       ; don't need cp 0, as the and sets the flags (saved 2bytes wooo!)
     jp z, loadAlternateShape1    
     ld (hl), SHAPE_CHAR_0
     jr drawNothing
@@ -629,7 +627,7 @@ drawNothing
     ld a, e
     cp 0  
     jp nz, drawShapeOuter
-    
+
     ret    
 
    

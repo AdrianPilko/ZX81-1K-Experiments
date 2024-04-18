f1e16e4d527c82be2f0bcf14ffae02d1fb2b4666
AdrianPilko <50658445+AdrianPilko@users.noreply.github.com>
2024-04-18 18:16:55 +0100

    simple clock using vsync
    currently just counts up, need to ability to set

---
+++ clock.asm
@@ -0,0 +1,322 @@
+; Copyright (c) 2024 Adrian Pilkington
+
+; Permission is hereby granted, free of charge, to any person obtaining a copy
+; of this software and associated documentation files (the "Software"), to deal
+; in the Software without restriction, including without limitation the rights
+; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+; copies of the Software, and to permit persons to whom the Software is
+; furnished to do so, subject to the following conditions:
+
+; The above copyright notice and this permission notice shall be included in all
+; copies or substantial portions of the Software.
+
+; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+; SOFTWARE.
+
+;;; Simple timer using vertical tv sync to time 
+;;;
+;;; https://youtube.com/@byteforever7829
+#define         DEFB .byte 
+#define         DEFW .word
+#define         EQU  .equ
+#define         ORG  .org
+CLS				EQU $0A2A
+
+VSYNCLOOP       EQU      2
+
+; character set definition/helpers
+__:				EQU	$00	;spacja
+_QT:			EQU	$0B	;"
+_PD:			EQU	$0C	;funt 
+_SD:			EQU	$0D	;$
+_CL:			EQU	$0E	;:
+_QM:			EQU	$0F	;?
+_OP:			EQU	$10	;(
+_CP:			EQU	$11	;)
+_GT:			EQU	$12	;>
+_LT:			EQU	$13	;<
+_EQ:			EQU	$14	;=
+_PL:			EQU	$15	;+
+_MI:			EQU	$16	;-
+_AS:			EQU	$17	;*
+_SL:			EQU	$18	;/
+_SC:			EQU	$19	;;
+_CM:			EQU	$1A	;,
+_DT:			EQU	$1B	;.
+_NL:			EQU	$76	;NEWLINE
+
+_BL             EQU $80; solid block
+
+_0				EQU $1C
+_1				EQU $1D
+_2				EQU $1E
+_3				EQU $1F
+_4				EQU $20
+_5				EQU $21
+_6				EQU $22
+_7				EQU $23
+_8				EQU $24
+_9				EQU $25
+_A				EQU $26
+_B				EQU $27
+_C				EQU $28
+_D				EQU $29
+_E				EQU $2A
+_F				EQU $2B
+_G				EQU $2C
+_H				EQU $2D
+_I				EQU $2E
+_J				EQU $2F
+_K				EQU $30
+_L				EQU $31
+_M				EQU $32
+_N				EQU $33
+_O				EQU $34
+_P				EQU $35
+_Q				EQU $36
+_R				EQU $37
+_S				EQU $38
+_T				EQU $39
+_U				EQU $3A
+_V				EQU $3B
+_W				EQU $3C
+_X				EQU $3D
+_Y				EQU $3E
+_Z				EQU $3F
+
+
+;;;; this is the whole ZX81 runtime system and gets assembled and 
+;;;; loads as it would if we just powered/booted into basic
+
+           ORG  $4009             ; assemble to this address
+                                                                
+VERSN:          DEFB 0
+E_PPC:          DEFW 2
+D_FILE:         DEFW Display
+DF_CC:          DEFW Display+1                  ; First character of display
+VARS:           DEFW Variables
+DEST:           DEFW 0
+E_LINE:         DEFW BasicEnd 
+CH_ADD:         DEFW BasicEnd+4                 ; Simulate SAVE "X"
+X_PTR:          DEFW 0
+STKBOT:         DEFW BasicEnd+5
+STKEND:         DEFW BasicEnd+5                 ; Empty stack
+BREG:           DEFB 0
+MEM:            DEFW MEMBOT
+UNUSED1:        DEFB 0
+DF_SZ:          DEFB 2
+S_TOP:          DEFW $0002                      ; Top program line number
+LAST_K:         DEFW $fdbf
+DEBOUN:         DEFB 15
+MARGIN:         DEFB 55
+NXTLIN:         DEFW Line2                      ; Next line address
+OLDPPC:         DEFW 0
+FLAGX:          DEFB 0
+STRLEN:         DEFW 0
+T_ADDR:         DEFW $0c8d
+SEED:           DEFW 0
+FRAMES:         DEFW $f5a3
+COORDS:         DEFW 0
+PR_CC:          DEFB $bc
+S_POSN:         DEFW $1821
+CDFLAG:         DEFB $40
+MEMBOT:         DEFB 0,0 ;  zeros
+UNUNSED2:       DEFW 0
+
+            ORG 16509       ;; we have to push the place in memory for this here becuase basic has 
+                    ;; to start at 16514 if memory was tight we could use the space between UNUSED2
+                    ;; and Line1 for variables
+
+Line1:          DEFB $00,$0a                    ; Line 10
+                DEFW Line1End-Line1Text         ; Line 10 length
+Line1Text:      DEFB $ea                        ; REM
+
+    
+
+preinit
+;; initialise variables that are once per game load/start
+
+initVariables
+
+
+;; initialise variables per game
+    xor a
+
+    ld (gameTime_Seconds), a
+    ld (gameTime_Minutes), a
+    ld (gameTimeCounterJIFFIES), a
+    
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;    
+mainLoop    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+	ld b,VSYNCLOOP
+waitForTVSync	
+	call vsync
+	djnz waitForTVSync
+    
+    call printTime
+
+    jp mainLoop
+    
+    ret   ; never gets here
+
+printTime    
+    ld bc, 370 
+    ld de, TimeText
+    call printstring
+    
+    ld a, (gameTime_Seconds)
+    ld de, 381
+    call print_number8bits    
+    
+    ld de, 380
+    ld hl, (DF_CC)    
+    add hl, de  
+    ld a, _CL
+    ld (hl), a
+    
+    ld a, (gameTime_Minutes)
+    ld de, 378    
+    call print_number8bits       
+   
+    ret
+  
+      
+; this prints at to any offset (stored in bc) from the top of the screen Display, using string in de
+printstring
+    push de ; preserve de
+    ld hl,Display
+    add hl,bc	
+printstring_loop
+    ld a,(de)
+    cp $ff
+    jp z,printstring_end
+    ld (hl),a
+    inc hl
+    inc de
+    jr printstring_loop
+printstring_end	
+    pop de  ; preserve de
+    ret  
+    
+   
+print_number8bits
+    ld hl, (DF_CC)    
+    add hl, de    
+    push af ;store the original value of a for later
+    and $f0 ; isolate the first digit
+    rra
+    rra
+    rra
+    rra
+    add a,$1c ; add 28 to the character code
+    ld (hl), a
+    inc hl
+    pop af ; retrieve original value of a
+    and $0f ; isolate the second digit
+    add a,$1c ; add 28 to the character code
+    ld (hl), a  
+    
+    ret
+
+    
+
+;check if TV synchro (FRAMES) happend
+vsync	
+	ld a,(FRAMES)
+	ld c,a
+sync
+	ld a,(FRAMES)
+	cp c
+	jr z,sync
+    
+    ld a, (gameTimeCounterJIFFIES)
+    inc a
+    cp 50    
+    jr z, skipJIFFIESUpdate
+    
+    ld (gameTimeCounterJIFFIES), a
+    
+    jr endOfVsync
+    
+skipJIFFIESUpdate
+    xor a
+    ld (gameTimeCounterJIFFIES), a  ; reset JIFFIES
+    ld a, (gameTime_Seconds)   ;; increment seconds
+    inc a
+    daa
+    cp $60
+    jr z, updateMinutes
+    
+    ld (gameTime_Seconds), a
+    jr endOfVsync
+updateMinutes    
+    xor a
+    ld (gameTime_Seconds), a
+    ld a, (gameTime_Minutes)    
+    inc a
+    daa
+    ld (gameTime_Minutes), a
+    
+endOfVsync        
+	ret
+
+    
+                DEFB $76                        ; Newline        
+Line1End
+Line2			DEFB $00,$14
+                DEFW Line2End-Line2Text
+Line2Text     	DEFB $F9,$D4                    ; RAND USR
+				DEFB $1D,$22,$21,$1D,$20        ; 16514                
+                DEFB $7E                        ; Number
+                DEFB $8F,$01,$04,$00,$00        ; Numeric encoding
+                DEFB $76                        ; Newline
+Line2End            
+endBasic
+                                                                
+Display        	DEFB $76                                                 				
+                DEFB  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,$76                     
+                DEFB  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,$76           
+                DEFB  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,$76           
+                DEFB  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,$76           
+                DEFB  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,$76           
+                DEFB  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,$76           
+                DEFB  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,$76           
+                DEFB  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,$76           
+                DEFB  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,$76           
+                DEFB  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,$76           
+                DEFB  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,$76           
+                DEFB  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,$76           
+                DEFB  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,$76           
+                DEFB  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,$76           
+                DEFB  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,$76           
+                DEFB  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,$76           
+                DEFB  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,$76           
+                DEFB  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,$76           
+                DEFB  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,$76           
+                DEFB  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,$76           
+                DEFB  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,$76           
+                DEFB  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,$76           
+                DEFB  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,$76           
+                DEFB  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,$76           
+
+Variables: 
+gameTime_Seconds
+    DEFB 0 
+gameTime_Minutes
+    DEFB 0
+gameTimeCounterJIFFIES  ; a nod to C64 1/60th of a second count reset at 60.
+    DEFB 0
+    
+TimeText
+    DEFB _T,_I,_M,_E,_EQ,$ff
+
+    
+VariablesEnd:   DEFB $80
+BasicEnd: 
+#END
